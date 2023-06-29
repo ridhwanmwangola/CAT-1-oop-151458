36cde65eb9980557f861bda2da875fa4b9abc4f8
ridhwanmwangola <138147194+ridhwanmwangola@users.noreply.github.com>
2023-06-29 19:40:08 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,32 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+public class Customers
+private string="1st name";
+private string="2nd name";
+private string="3rd name";
+ int=DOB;
+ int=DOR;
+ string="nationality"
+{
+    public void Order {
+        int OrderID;
+        int Orderdate;
+        
+OrderID=35;
+Orderdate=25;
+    }
+     public void Transaction{
+         int amountpaid;
+         int amountowed;
+         int change;
+         int receiptnumber;
+         
+         amountpaid=5000;
+         amountowed=0;
+         change=0;
+         receuiptnumber=008;
+         
+         
+     }
+}
